07f5c4ffac2ab17749c4fc6fa0377c53bb3c4d38
DaniruDesilva <daniru.desilva1019@gmail.com>
2024-04-05 01:19:20 +0530

    home page build

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -10,6 +10,125 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    double myHeight = MediaQuery.of(context).size.height;
+    double myWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: myHeight,
+        width: myWidth,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color.fromARGB(255, 249, 214, 73), Color(0xffFBC700)]),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: myHeight * 0.03),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: myWidth * 0.02, vertical: myHeight * 0.005),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: const Text(
+                      'Main portfolio',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const Text(
+                    'Top 10 coins',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  const Text(
+                    'Exprimental',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: myWidth * 0.07),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    '\$ 7,466.20',
+                    style: TextStyle(fontSize: 35),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(myWidth * 0.02),
+                    height: myHeight * 0.04,
+                    width: myWidth * 0.1,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white.withOpacity(0.5)),
+                    child: Image.asset(
+                      'assets/icons/5.1.png',
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: myWidth * 0.07),
+              child: const Row(
+                children: [
+                  Text(
+                    '+162% all time',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: myHeight * 0.02,
+            ),
+            Container(
+              height: myHeight * 0.7,
+              width: myWidth,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  )),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: myHeight * 0.02,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: myWidth * 0.08),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Assets',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Icon(Icons.add)
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: 20,
+                        itemBuilder: (context, index) {
+                          return const Text('data');
+                        }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
