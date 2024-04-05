bed7078b847d60c673b8cfb51c6bb0c763e6eedd
DaniruDesilva <daniru.desilva1019@gmail.com>
2024-04-05 21:59:12 +0530

    changes

--- lib/view/components/item2.dart
+++ lib/view/components/item2.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_pulse/view/select_coin.dart';
 import 'package:flutter/material.dart';
 
 class Item2 extends StatelessWidget {
@@ -10,55 +11,62 @@ class Item2 extends StatelessWidget {
     double myWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: myWidth * 0.03),
-      child: Container(
-        padding: EdgeInsets.only(
-            left: myWidth * 0.03,
-            right: myWidth * 0.06,
-            top: myHeight * 0.02,
-            bottom: myHeight * 0.02),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(color: Colors.grey)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: myHeight * 0.035,
-              child: Image.network(item.image),
-            ),
-            SizedBox(
-              height: myHeight * 0.01,
-            ),
-            Text(
-              item.id,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              children: [
-                Text(
-                  item.priceChange24H.toString().contains('-')
-                      ? '-\$${item.priceChange24H.toStringAsFixed(2).toString().replaceAll('-', '')}'
-                      : "\$${item.priceChange24H.toStringAsFixed(2)}",
-                  style: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey),
-                ),
-                SizedBox(
-                  width: myWidth * 0.02,
-                ),
-                Text(
-                  item.marketCapChangePercentage24H.toStringAsFixed(2) + '%',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                      color: item.marketCapChangePercentage24H >= 0
-                          ? Colors.green
-                          : Colors.red),
-                ),
-              ],
-            )
-          ],
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const SelectCoin()));
+        },
+        child: Container(
+          padding: EdgeInsets.only(
+              left: myWidth * 0.03,
+              right: myWidth * 0.06,
+              top: myHeight * 0.02,
+              bottom: myHeight * 0.02),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.grey)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: myHeight * 0.035,
+                child: Image.network(item.image),
+              ),
+              SizedBox(
+                height: myHeight * 0.01,
+              ),
+              Text(
+                item.id,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                children: [
+                  Text(
+                    item.priceChange24H.toString().contains('-')
+                        ? '-\$${item.priceChange24H.toStringAsFixed(2).toString().replaceAll('-', '')}'
+                        : "\$${item.priceChange24H.toStringAsFixed(2)}",
+                    style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey),
+                  ),
+                  SizedBox(
+                    width: myWidth * 0.02,
+                  ),
+                  Text(
+                    item.marketCapChangePercentage24H.toStringAsFixed(2) + '%',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        color: item.marketCapChangePercentage24H >= 0
+                            ? Colors.green
+                            : Colors.red),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
