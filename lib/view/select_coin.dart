05915b7e4a173bf177a76ddb138195c284847d2c
DaniruDesilva <daniru.desilva1019@gmail.com>
2024-04-05 21:58:46 +0530

    Select Coin page

---
+++ lib/view/select_coin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectCoin extends StatefulWidget {
+  const SelectCoin({super.key});
+
+  @override
+  State<SelectCoin> createState() => _SelectCoinState();
+}
+
+class _SelectCoinState extends State<SelectCoin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
