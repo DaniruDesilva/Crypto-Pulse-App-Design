92c3d7de8affa3ec26ef9beba9e7c805f47137dc
Daniru De Silva <165991056+DaniruDesilva@users.noreply.github.com>
2024-04-07 13:49:17 +0530

    Changes

--- lib/view/select_coin.dart
+++ lib/view/select_coin.dart
@@ -17,15 +17,13 @@ class SelectCoin extends StatefulWidget {
 }
 
 class _SelectCoinState extends State<SelectCoin> {
-
   late TrackballBehavior trackballBehavior;
-
-
 
   @override
   void initState() {
     getChart();
-    
+    trackballBehavior = TrackballBehavior(
+        enable: true, activationMode: ActivationMode.singleTap);
     super.initState();
   }
 
@@ -103,84 +101,267 @@ class _SelectCoinState extends State<SelectCoin> {
               ),
             ),
             const Divider(),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: myWidth * 0.05, vertical: myHeight * 0.02),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
+            Expanded(
+                child: ListView(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: myWidth * 0.05, vertical: myHeight * 0.02),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
-                        'Low',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey),
+                      Column(
+                        children: [
+                          const Text(
+                            'Low',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey),
+                          ),
+                          SizedBox(
+                            height: myHeight * 0.01,
+                          ),
+                          Text(
+                            '\$${widget.selectItem.low24H.toString()}',
+                            style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: myHeight * 0.01,
+                      Column(
+                        children: [
+                          const Text(
+                            'High',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey),
+                          ),
+                          SizedBox(
+                            height: myHeight * 0.01,
+                          ),
+                          Text(
+                            '\$${widget.selectItem.high24H.toString()}',
+                            style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black),
+                          ),
+                        ],
                       ),
-                      Text(
-                        '\$${widget.selectItem.low24H.toString()}',
-                        style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black),
+                      Column(
+                        children: [
+                          const Text(
+                            'Vol',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey),
+                          ),
+                          SizedBox(
+                            height: myHeight * 0.01,
+                          ),
+                          Text(
+                            '\$${'${widget.selectItem.totalVolume}M'}',
+                            style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                  Column(
+                ),
+                SizedBox(
+                  height: myHeight * 0.02,
+                ),
+                SizedBox(
+                  height: myHeight * 0.4,
+                  width: myWidth,
+                  // color: Colors.amber,
+                  child: isRefresh == true
+                      ? const Center(
+                          child: CircularProgressIndicator(
+                            color: Color(0xffFBC700),
+                          ),
+                        )
+                      : SfCartesianChart(
+                          trackballBehavior: trackballBehavior,
+                          zoomPanBehavior: ZoomPanBehavior(
+                              enablePanning: true, zoomMode: ZoomMode.x),
+                          series: <CandleSeries>[
+                            CandleSeries<ChartModel, int>(
+                              enableSolidCandles: true,
+                              enableTooltip: true,
+                              bullColor: Colors.green,
+                              bearColor: Colors.red,
+                              dataSource: itemChart,
+                              xValueMapper: (ChartModel sales, _) => sales.time,
+                              lowValueMapper: (ChartModel sales, _) =>
+                                  sales.low,
+                              highValueMapper: (ChartModel sales, _) =>
+                                  sales.high,
+                              openValueMapper: (ChartModel sales, _) =>
+                                  sales.open,
+                              closeValueMapper: (ChartModel sales, _) =>
+                                  sales.close,
+                              animationDuration: 55,
+                            )
+                          ],
+                        ),
+                ),
+                SizedBox(
+                  height: myHeight * 0.01,
+                ),
+                Center(
+                  child: SizedBox(
+                    height: myHeight * 0.035,
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: text.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: myWidth * 0.02),
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  textBool = [
+                                    false,
+                                    false,
+                                    false,
+                                    false,
+                                    false,
+                                    false
+                                  ];
+                                  textBool[index] = true;
+                                });
+                                setDays(text[index]);
+                                getChart();
+                              },
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: myWidth * 0.03,
+                                    vertical: myHeight * 0.005),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: textBool[index] == true
+                                      ? const Color(0xffFBC700).withOpacity(0.3)
+                                      : Colors.transparent,
+                                ),
+                                child: Text(
+                                  text[index],
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                ),
+                SizedBox(
+                  height: myHeight * 0.04,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: myWidth * 0.06),
+                  child: const Text(
+                    'News',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: myWidth * 0.06, vertical: myHeight * 0.01),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text(
-                        'High',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey),
+                      const Expanded(
+                        child: Text(
+                          'vhabejvehbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbonnfvoanerbaobnebeav vr ka re fffffffffffffffffffffffffffffffffffffffffffffffffrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrssssssssssssssssssssssssssssssssssssssssssssssssttttttttttttttttttttttttttttttttttttttttttttttnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(color: Colors.grey, fontSize: 17),
+                        ),
                       ),
                       SizedBox(
-                        height: myHeight * 0.01,
-                      ),
-                      Text(
-                        '\$${widget.selectItem.high24H.toString()}',
-                        style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black),
-                      ),
+                        width: myWidth * 0.25,
+                        child: CircleAvatar(
+                          radius: myHeight * 0.04,
+                          backgroundImage:
+                              const AssetImage('assets/images/6456.PNG'),
+                        ),
+                      )
                     ],
                   ),
-                  Column(
-                    children: [
-                      const Text(
-                        'Vol',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey),
-                      ),
-                      SizedBox(
-                        height: myHeight * 0.01,
-                      ),
-                      Text(
-                        '\$${'${widget.selectItem.totalVolume}M'}',
-                        style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                )
+              ],
+            )),
             SizedBox(
-              height: myHeight * 0.4,
+              height: myHeight * 0.1,
               width: myWidth,
               // color: Colors.amber,
-              child: SfCartesianChart(
-                trackballBehavior: ,
+              child: Column(
+                children: [
+                  const Divider(),
+                  SizedBox(
+                    height: myHeight * 0.01,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: myWidth * 0.05,
+                      ),
+                      Expanded(
+                        flex: 5,
+                        child: Container(
+                          padding:
+                              EdgeInsets.symmetric(vertical: myHeight * 0.015),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: const Color(0xffFBC700)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                size: myHeight * 0.02,
+                              ),
+                              const Text(
+                                'Add to portfolio',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: myWidth * 0.05,
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          padding:
+                              EdgeInsets.symmetric(vertical: myHeight * 0.012),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.grey.withOpacity(0.2)),
+                          child: Image.asset(
+                            'assets/icons/3.1.png',
+                            height: myHeight * 0.03,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: myWidth * 0.05,
+                      ),
+                    ],
+                  )
+                ],
               ),
             )
           ],
@@ -189,16 +370,60 @@ class _SelectCoinState extends State<SelectCoin> {
     ));
   }
 
+  List<String> text = ['D', 'W', 'M', '3M', '6M', 'Y'];
+  List<bool> textBool = [false, false, true, false, false, false];
+
+  int days = 30;
+
+  setDays(String txt) {
+    if (txt == 'D') {
+      setState(() {
+        days = 1;
+      });
+    } else if (txt == 'W') {
+      setState(() {
+        days = 7;
+      });
+    } else if (txt == 'M') {
+      setState(() {
+        days = 30;
+      });
+    } else if (txt == '3M') {
+      setState(() {
+        days = 90;
+      });
+    } else if (txt == '6M') {
+      setState(() {
+        days = 180;
+      });
+    } else if (txt == 'Y') {
+      setState(() {
+        days = 365;
+      });
+    }
+  }
+
   List<ChartModel>? itemChart;
+
+  bool isRefresh = true;
 
   Future<void> getChart() async {
     String url =
-        'https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=1';
+        '${'https://api.coingecko.com/api/v3/coins/' + widget.selectItem.id}/ohlc?vs_currency=usd&days=$days';
+
+    setState(() {
+      isRefresh = true;
+    });
 
     var response = await http.get(Uri.parse(url), headers: {
       "Content-Type": "application/json",
       "Accept": "application/json",
     });
+
+    setState(() {
+      isRefresh = false;
+    });
+
     if (response.statusCode == 200) {
       Iterable x = json.decode(response.body);
       List<ChartModel> modelList =
